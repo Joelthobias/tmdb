3027ffe32c853f117d32a861899633acd76f491f
joel <joelthobias447@gmail.com>
2024-01-20 06:26:22 +0530

    aaa

---
+++ src/modules/dashboard/services/fetchMovies.tsx
@@ -0,0 +1,25 @@
+const fetch = require('node-fetch');
+const fetchTrendingMovies = async () => {
+
+const url = 'https://api.themoviedb.org/3/trending/all/day?language=en-US';
+try {
+    console.log("Api callied to fetch");
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MmM1ZGRhNTMwZWJiYWM1ZWQ4NmYxOThiMjVhOTJkYiIsInN1YiI6IjYyOGI5MWFhMGI3MzE2MDA2NjExNWRlZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JPwOZcvn_pjPalLjyprRI7D7n-hUXATYjHvrZvkH-LY'
+      }
+    };
+    
+
+    const response = await fetch(url, options);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export { fetchTrendingMovies };
---
+++ src/modules/dashboard/services/serachMovies.tsx
@@ -0,0 +1,29 @@
+
+interface SearchResult {
+    id: number;
+    title: string;
+  }
+  
+  export const searchMovies = async (query: string): Promise<SearchResult[]> => {
+    const url = `https://api.themoviedb.org/3/search/movie?query=${query}&page=1`;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MmM1ZGRhNTMwZWJiYWM1ZWQ4NmYxOThiMjVhOTJkYiIsInN1YiI6IjYyOGI5MWFhMGI3MzE2MDA2NjExNWRlZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JPwOZcvn_pjPalLjyprRI7D7n-hUXATYjHvrZvkH-LY',
+      },
+    };
+  
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      console.log(data);
+      
+      return data.results;
+      
+    } catch (error) {
+      console.error('Error searching for movies:', error);
+      throw error;
+    }
+  };
+  
